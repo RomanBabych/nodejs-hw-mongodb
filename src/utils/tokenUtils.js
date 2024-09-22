420d9e54994dba141aef2a96f4a68dd6385ea4c9
RomanBabych <rrroma.babi42017@gmail.com>
2024-09-22 21:01:09 +0300

    homework try

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -8,6 +8,8 @@ export const generateTokens = (userId) => {
   const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
   const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
+  console.log(ACCESS_TOKEN_SECRET);
+
   if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
     throw new Error('Token secrets are not defined');
   }
